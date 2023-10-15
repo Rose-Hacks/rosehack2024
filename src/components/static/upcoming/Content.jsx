819be83ea3696e874b9f177d47c32738b7113602
menthy-wu <wumengxuan0728@gmail.com>
2023-10-15 02:23:57 -0700

    mobile

--- src/components/static/upcoming/Content.jsx
+++ src/components/static/upcoming/Content.jsx
@@ -10,13 +10,13 @@ const Content = () => {
       <p className="w-full text-center font-righteous text-3xl md:text-4xl drop-shadow-pink-200">
         COMING SOON
       </p>
-      <div className="backdrop-blur-md rounded-3xl my-4">
+      <div className="backdrop-blur-md rounded-3xl my-5">
         <p className="font-orbitron text-center mb-0 p-2 border-[1px] rounded-t-3xl border-white/20 w-full text-lg md:text-2xl text-white bg-gradient-to-r from-white/20 to-white/0">
           January 13 - 14
         </p>
         <div className="p-3 border-t-0 border-[1px] rounded-b-3xl border-white/20 w-full text-white bg-gradient-to-b from-black/50 to-black/0 font-orbitron flex flex-col items-center">
           <Countdown />
-          <div className="my-3 flex w-full gap-3">
+          <div className="my-3 flex flex-col md:flex-row w-full gap-3">
             <Link
               link="https://forms.gle/Pr8HMgaLWunGjWVv6"
               text="interest form"
