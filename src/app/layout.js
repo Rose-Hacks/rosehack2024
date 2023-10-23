37a097ef23aaf4f958a8b5440b341964bb213b7a
Divyank Shah <divyank.shah.2016@gmail.com>
2023-10-23 15:11:45 -0700

    merge

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from "react-hot-toast";
-import { Orbitron, Montserrat, Righteous } from "next/font/google";
+import { Orbitron, Montserrat, Righteous, Poppins } from "next/font/google";
 
 const orbitron = Orbitron({ subsets: ["latin"], variable: "--font-orbitron" });
 const righteous = Righteous({
@@ -12,10 +12,18 @@ const righteous = Righteous({
   variable: "--font-righteous",
   weight: ["400"],
 });
+
 const montserrat = Montserrat({
   weight: ["variable"],
   subsets: ["latin"],
   variable: "--font-montserrat",
+});
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
 });
 
 export default function RootLayout({ children, session }) {
@@ -27,7 +35,7 @@ export default function RootLayout({ children, session }) {
         className="h-full"
       >
         <body
-          className={`${orbitron.variable} ${montserrat.variable} ${righteous.variable} bg-black`}
+          className={`${orbitron.variable} ${montserrat.variable} ${righteous.variable} ${poppins.variable} bg-black flex flex-col lg:flex-row h-full`}
         >
           <Toaster />
           {children}
