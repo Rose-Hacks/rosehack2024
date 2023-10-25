2e4f8b303c04552a3b33e15545157b9913cfe8ca
theo <theobfernandez@gmail.com>
2023-10-24 23:12:28 -0700

    Finish board component

--- src/components/static/prelive/Board.jsx
+++ src/components/static/prelive/Board.jsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 
-const Board = ({position, name,image}) => {
+const Board = ({ position, name, image }) => {
   return (
-		<div className="font-montserrat text-center text-hackathon-tags-white flex-col">
-			<Image className="rounded-full mb-1" src={image} />
-			<div>{position}</div>
-			<div>{name}</div>
-		</div>
-	);
+    <div className="font-montserrat text-center text-hackathon-tags-white flex-col">
+      <Image className="rounded-full mb-1" src={image} />
+      <div>{position}</div>
+      <div>{name}</div>
+    </div>
+  );
 };
 
 export default Board;
