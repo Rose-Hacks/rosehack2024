320736a175f2bfd49817c55f266583bcf7b0470a
SafeDuck <schopra579@gmail.com>
2023-10-25 14:59:48 -0700

    add files and assets

--- src/components/static/prelive/board/Board.jsx
+++ src/components/static/prelive/board/Board.jsx
@@ -1,5 +1,13 @@
-const Board = () => {
-  return <div>Board</div>;
+import Image from "next/image";
+
+const Board = ({ position, name, image }) => {
+  return (
+    <div className="font-montserrat text-center text-hackathon-tags-white flex-col">
+      <Image className="rounded-full mb-1" src={image} />
+      <div>{position}</div>
+      <div>{name}</div>
+    </div>
+  );
 };
 
 export default Board;
