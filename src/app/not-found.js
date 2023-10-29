039d926dee9b704a8c7c95c794ed89aef08da742
fardinzam <fardinzaman51@gmail.com>
2023-10-28 20:15:51 -0700

    added text, need arrows

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,5 +1,6 @@
 import img from "../../public/svg/blue-robot.svg";
 import Image from "next/image";
+import Title from "@/components/static/prelive/TItle";
 
 const error = () => {
   return (
@@ -14,6 +15,7 @@ const error = () => {
       <p className="text-center text-sm md:text-lg text-rosehack-blue font-montserrat">
         The page you are looking for does not seem to exist.
       </p>
+      <Title label="Meet The Team" />
     </div>
   );
 };
