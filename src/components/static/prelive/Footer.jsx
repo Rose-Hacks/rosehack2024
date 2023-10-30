dcea8b5a65877b05fa5e544f40334fab51e9f545
seanquiambao <slquiambao03@gmail.com>
2023-10-29 23:53:28 -0700

    footer inprogress

--- src/components/static/prelive/Footer.jsx
+++ src/components/static/prelive/Footer.jsx
@@ -1,5 +1,15 @@
+import Image from "next/image";
+import LOGO from "../../../../public/LOGO.svg";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="flex w-screen h-full bg-black grid-cols-4">
+      <div className="col-span-3">
+        <Image src={LOGO} className="w-1/6 mt-12 mb-2" alt={`Logo`} />
+      </div>
+      <div className="text-white"> SOCIALS</div>
+    </div>
+  );
 };
 
 export default Footer;
