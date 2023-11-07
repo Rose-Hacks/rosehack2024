9b941cccd9785ab5b233d6bb189eac2cdb1c103f
menthy-wu <wumengxuan0728@gmail.com>
2023-11-06 16:20:30 -0800

    fix form style

--- src/components/dynamic/form/Form.jsx
+++ src/components/dynamic/form/Form.jsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 import Status from "./Status";
 import Questions from "./Questions";
 import Confirmation from "./Confirmation";
+import Image from "next/image";
+import LOGO from "../../../../public/LOGO.svg";
 
 const Form = ({
   object,
@@ -20,12 +22,14 @@ const Form = ({
   );
 
   return (
-    <div className="w-full h-full overflow-scroll flex flex-col items-center font-poppins">
+    <div className="w-full h-full overflow-scroll flex flex-col items-center font-montserrat text-white">
+      <Image src={LOGO} className="w-1/12 mt-12 mb-2" alt={`Logo`} />
+      <div className="text-4xl font-thin font-advent">ROSEHACK 2024</div>
       <div className="w-10/12 md:w-1/2 xl:w-1/3 my-5">
-        <p className="text-xl bg-hackathon-green-300 font-semibold px-4 py-2 rounded-t-xl m-0">
+        <p className="text-xl bg-gradient-to-r from-rosehack-pink-200/40 to-rosehack-blue/40 font-montserrat px-4 py-2 m-0">
           {header}
         </p>
-        <div className="rounded-b-xl bg-white p-3">
+        <div className="bg-white/10 p-3 backdrop-blur-md">
           <div className="grid grid-cols-1 gap-3">
             {state === 0 ? (
               <Status object={object} statuses={statuses} setState={setState} />
