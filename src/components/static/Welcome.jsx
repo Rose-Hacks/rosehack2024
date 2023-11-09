6ecc5223004ee1f623e7d641bf1a28bd8ca41b2e
menthy-wu <wumengxuan0728@gmail.com>
2023-11-08 23:24:41 -0800

    mobile

--- src/components/static/Welcome.jsx
+++ src/components/static/Welcome.jsx
@@ -1,17 +1,19 @@
 import Image from "next/image";
 import bg from "../../../public/svg/welcome/bg.svg";
+import bgsm from "../../../public/svg/welcome/bgsm.svg";
 import landingCircleBlue from "../../../public/svg/landing/landing-circle-blue.svg";
 
 const Welcome = () => {
   return (
     <div className="relative w-fit flex items-center justify-center">
-      <Image src={bg} className="w-3/5" />
-      <div className="absolute w-1/5 text-white text-center text-xl font-montserrat">
+      <Image src={bgsm} className="w-full block md:hidden" />
+      <Image src={bg} className="w-4/5 hidden md:block" />
+      <div className="absolute w-1/3 md:w-1/4 lg:w-1/5 text-white text-center text-xs md:text-xl font-montserrat">
         Rose Hack welcomes both undergraduate and high school students for a
         lively weekend filled with innovation, discovery, networking,
         mentorship, and so much more!
       </div>
-      <div className="w-2/5 absolute right-1/2 translate-x-1/2 top-1/2 -translate-y-1/2">
+      <div className="w-4/5 md:w-1/2 absolute right-1/2 translate-x-1/2 top-1/2 -translate-y-1/2">
         <Image
           alt="landing circle"
           src={landingCircleBlue}
