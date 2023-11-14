b693499acf1e8f3959255ac94b7177da9d06a293
Divyank Shah <divyank.shah.2016@gmail.com>
2023-11-13 17:25:41 -0800

    fix test

--- cypress/e2e/admin/admins/Filter.cy.js
+++ cypress/e2e/admin/admins/Filter.cy.js
@@ -24,22 +24,22 @@ describe("Admin Filters", () => {
   });
 
   it("Click Filters", () => {
-    cy.get('[data-cy="pending-filter"]').click();
+    cy.get('[data-cy="pending-filter"]').click({ force: true });
     cy.get('[data-cy="pending-filter"]')
       .get("div")
       .should("have.class", "text-hackathon-blue-100", "bg-white");
-    cy.get('[data-cy="reject-filter"]').click();
+    cy.get('[data-cy="reject-filter"]').click({ force: true });
     cy.get('[data-cy="reject-filter"]')
       .get("div")
       .should("have.class", "text-hackathon-blue-100", "bg-white");
-    cy.get('[data-cy="accept-filter"]').click();
+    cy.get('[data-cy="accept-filter"]').click({ force: true });
     cy.get('[data-cy="accept-filter"]')
       .get("div")
       .should("have.class", "text-hackathon-blue-100", "bg-white");
   });
 
   it("Click Pending", () => {
-    cy.get('[data-cy="pending-filter"]').click();
+    cy.get('[data-cy="pending-filter"]').click({ force: true });
     admins.forEach((admin) => {
       if (admin.status === 0)
         cy.get(`[data-cy="${admin.uid}"]`).should("not.exist");
@@ -48,7 +48,7 @@ describe("Admin Filters", () => {
   });
 
   it("Click Rejected", () => {
-    cy.get('[data-cy="reject-filter"]').click();
+    cy.get('[data-cy="reject-filter"]').click({ force: true });
     admins.forEach((admin) => {
       if (admin.status === -1)
         cy.get(`[data-cy="${admin.uid}"]`).should("not.exist");
@@ -57,7 +57,7 @@ describe("Admin Filters", () => {
   });
 
   it("Click Accepted", () => {
-    cy.get('[data-cy="accept-filter"]').click();
+    cy.get('[data-cy="accept-filter"]').click({ force: true });
     admins.forEach((admin) => {
       if (admin.status === 1)
         cy.get(`[data-cy="${admin.uid}"]`).should("not.exist");
@@ -66,8 +66,8 @@ describe("Admin Filters", () => {
   });
 
   it("Click 2 Filters", () => {
-    cy.get('[data-cy="accept-filter"]').click();
-    cy.get('[data-cy="pending-filter"]').click();
+    cy.get('[data-cy="accept-filter"]').click({ force: true });
+    cy.get('[data-cy="pending-filter"]').click({ force: true });
     admins.forEach((admin) => {
       if (admin.status === 1 || admin.status === 0)
         cy.get(`[data-cy="${admin.uid}"]`).should("not.exist");
