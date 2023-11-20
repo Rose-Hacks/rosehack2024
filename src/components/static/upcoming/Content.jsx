2c213e4212f5e0c9f380985e5e1d6cf8b2904925
seanquiambao <slquiambao03@gmail.com>
2023-11-19 16:19:09 -0800

    countdown responsiveness

--- src/components/static/upcoming/Content.jsx
+++ src/components/static/upcoming/Content.jsx
@@ -3,14 +3,14 @@ import Countdown from "../Countdown";
 
 const Content = () => {
   return (
-    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white">
+    <div className="absolute top-1/2 -translate-y-1/2 text-white w-full flex flex-col items-center">
       <p className="w-full text-center font-advent text-4xl md:text-5xl drop-shadow-pink-200">
         ROSEHACK 2024
       </p>
       <p className="w-full text-center font-advent text-3xl md:text-4xl drop-shadow-blue">
         COMING SOON
       </p>
-      <div className="backdrop-blur-md rounded-3xl my-5">
+      <div className="w-9/12 md:w-fit backdrop-blur-md rounded-3xl my-5">
         <p className="font-orbitron text-center mb-0 p-2 border-[1px] rounded-t-3xl border-white/20 w-full text-lg md:text-2xl text-white bg-gradient-to-r from-white/20 to-white/0">
           January 13 - 14
         </p>
