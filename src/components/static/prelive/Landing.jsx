9730951bd6c53abf7a06ace11a1d134ba568e6dc
menthy-wu <wumengxuan0728@gmail.com>
2023-11-20 22:32:06 -0800

    add parallax

--- src/components/static/prelive/Landing.jsx
+++ src/components/static/prelive/Landing.jsx
@@ -3,26 +3,33 @@ import building from "../../../../public/svg/building.svg";
 import LandingCircle from "../upcoming/LandingCircle.jsx";
 import blackBuilding from "../../../../public/svg/blackbuilding.svg";
 import Content from "./Content.jsx";
+import { Parallax } from "react-scroll-parallax";
 
 const Landing = () => {
   return (
     <div className="w-full relative overflow-hidden max-h-screen">
-      <Image
-        src={building}
-        alt="building"
-        className="w-full opacity-25 -scale-x-100 md:mt-0 mt-[35%]"
-      />
-      <LandingCircle />
-      <Image
-        src={building}
-        alt="building"
-        className="w-full relative -mt-[35%] md:-mt-[75%]"
-      />
-      <Image
-        src={blackBuilding}
-        alt="building"
-        className="w-full relative md:-mt-[50%] -mt-[35%]"
-      />
+      <Parallax speed={-50}>
+        <Image
+          src={building}
+          alt="building"
+          className="w-full opacity-25 -scale-x-100 md:mt-0 mt-[35%]"
+        />
+      </Parallax>
+      <Parallax speed={-30}>
+        <LandingCircle />
+        <Image
+          src={building}
+          alt="building"
+          className="w-full relative -mt-[35%] md:-mt-[75%]"
+        />
+      </Parallax>
+      <Parallax speed={0}>
+        <Image
+          src={blackBuilding}
+          alt="building"
+          className="w-full relative md:-mt-[50%] -mt-[35%]"
+        />
+      </Parallax>
       <Content />
     </div>
   );
--- src/components/static/prelive/Prelive.jsx
+++ src/components/static/prelive/Prelive.jsx
@@ -1,15 +1,19 @@
+"use client";
 import Welcome from "../Welcome";
 import About from "../about/About";
 import Landing from "./Landing";
 import Footer from "./Footer";
+import { ParallaxProvider } from "react-scroll-parallax";
 
 const Prelive = () => {
   return (
     <div className="w-full bg-blur">
-      <Landing />
-      <About />
-      <Welcome />
-      <Footer />
+      <ParallaxProvider>
+        <Landing />
+        <About />
+        <Welcome />
+        <Footer />
+      </ParallaxProvider>
     </div>
   );
 };
