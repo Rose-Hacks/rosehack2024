70f9a230dcedac2301907e9d7a54cff88f0f66dc
fardinzam <fardinzaman51@gmail.com>
2023-11-21 23:41:40 -0800

    navbar wip

--- src/components/static/Navigation.jsx
+++ src/components/static/Navigation.jsx
@@ -1,5 +1,13 @@
+import Image from "next/image";
+import LOGO from "../../../public/LOGO.svg";
+
 const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <div className="flex flex-row bg-rosehack-teal h-1/6 align-middle">
+      <Image src={LOGO} alt="logo" width={"70"} height={"70"} />
+      Home About Team Judges FAQ
+    </div>
+  );
 };
 
 export default Navigation;
