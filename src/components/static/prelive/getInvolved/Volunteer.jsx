7afe22997ee3958ed9543d9d01e3895ccdf15a87
Divyank Shah <divyank.shah.2016@gmail.com>
2023-11-25 20:20:35 -0800

    fix get involved section

--- src/components/static/prelive/getInvolved/Volunteer.jsx
+++ src/components/static/prelive/getInvolved/Volunteer.jsx
@@ -3,20 +3,23 @@ import Image from "next/image";
 import PinkRobot from "public/svg/pink-robot.svg";
 const Volunteer = () => {
   return (
-    <div className="font-montserrat flex">
+    <div className="font-montserrat flex w-10/12">
       <div className="flex-col text-hackathon-tags-white text-right">
-        <div className="text-5xl">Become a Volunteer</div>
+        <div className="text-2xl">Become a Volunteer</div>
         <div className="pt-3 pl-10">
           <div className="flex">
             <div className="w-3/4"></div>
-            <p className="text-2xl m-0 text-right px-0 pt-0 pr-0 pb-3">
-              In cursus turpis massa tincidunt dui ut ornare lectus sit. Viverra
-              vitae congue eu consequat ac felis.
+            <p className="text-lg m-0 text-right px-0 pt-0 pr-0 pb-3">
+              Are you ready to be a driving force behind groundbreaking ideas
+              and female empowerment in STEM? Look no further! Rose Hack, the
+              premier women-centric hackathon in the Inland Empire, is calling
+              on enthusiastic individuals to join us as volunteers for an
+              unforgettable experience on January 20th-21st, 2024!
             </p>
           </div>
           <div className="flex justify-end">
             <Link
-              text="register"
+              text="volunteer"
               link="/form/volunteer"
               fromColor="from-[#C44B8C]"
               toColor="to-[#E77FF8]"
