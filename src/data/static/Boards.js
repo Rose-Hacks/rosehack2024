612e1e37c2d7117513f29252386f6603c4eaf340
SafeDuck <schopra579@gmail.com>
2023-11-29 01:19:38 -0800

    boards

--- src/data/static/Boards.js
+++ src/data/static/Boards.js
@@ -1,7 +1,82 @@
 export const boardMembers = [
   {
-    position: "",
-    name: "",
+    position: "Director",
+    name: "Angie Bermudez",
+    image: Placeholder,
+  },
+  {
+    position: "Director",
+    name: "Surabhi Tadvalkar",
+    image: Placeholder,
+  },
+  {
+    position: "Operations",
+    name: "Annabelle Le",
+    image: Placeholder,
+  },
+  {
+    position: "Operations",
+    name: "Hriyai Srinath",
+    image: Placeholder,
+  },
+  {
+    position: "Operations",
+    name: "Sneha Panda",
+    image: Placeholder,
+  },
+  {
+    position: "Operations",
+    name: "David Ryan",
+    image: Placeholder,
+  },
+  {
+    position: "Marketing",
+    name: "Katty Ramos",
+    image: Placeholder,
+  },
+  {
+    position: "Marketing",
+    name: "Tianna Huang",
+    image: Placeholder,
+  },
+  {
+    position: "Marketing",
+    name: "Eliot Lee",
+    image: Placeholder,
+  },
+  {
+    position: "Sponsorship",
+    name: "Laura Canon",
+    image: Placeholder,
+  },
+  {
+    position: "Sponsorship",
+    name: "Rama Chunduri",
+    image: Placeholder,
+  },
+  {
+    position: "Sponsorship",
+    name: "Samarth Srinivasa",
+    image: Placeholder,
+  },
+  {
+    position: "Sponsorship",
+    name: "Yuchen Zhu",
+    image: Placeholder,
+  },
+  {
+    position: "Software Engineering",
+    name: "Menthy Wu",
+    image: Placeholder,
+  },
+  {
+    position: "Software Engineering",
+    name: "Divyank Shah",
+    image: Placeholder,
+  },
+  {
+    position: "Software Engineering",
+    name: "Sachin Chopra",
     image: Placeholder,
   },
 ];
