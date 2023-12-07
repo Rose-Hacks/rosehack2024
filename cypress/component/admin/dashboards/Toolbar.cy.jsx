f52dc8f8a80e04f71356ec86bb7aa32d34746967
theo <theobfernandez@gmail.com>
2023-12-06 19:04:34 -0800

    .

--- cypress/component/admin/dashboards/Toolbar.cy.jsx
+++ cypress/component/admin/dashboards/Toolbar.cy.jsx
@@ -93,12 +93,31 @@ describe("Toolbar", () => {
         />
       );
     };
-
+    cy.intercept("GET", `/api/admins`, {
+      fixture: `admins.json`,
+    }).as("GET");
+    cy.intercept("PUT", `/api/admins`, { message: "OK", status: 200 }).as(
+      "GET"
+    );
     cy.mount(
       <MockNextRouter>
         <Parent />
       </MockNextRouter>
     );
+
+    cy.get('[data-cy="delete"]')
+      .click()
+      .then(
+        () =>
+          expect(admins.every((admin) => admin.selected === false)).to.be.true
+      );
+    cy.get('[data-cy="checkbox-bg"]')
+      .click()
+      .should("have.class", "bg-transparent");
+    cy.get('[data-cy="delete"]').click();
+    cy.get('[data-cy="confirm-button"]')
+      .click()
+      .then(() => expect(admins).to.be.empty);
   });
 
   it("Reject all", () => {
