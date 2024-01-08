c3fb8894f37c9e4e2d16acff5f8d039aa18b403c
menthy-wu <wumengxuan0728@gmail.com>
2024-01-07 20:59:26 -0800

    mock calendar api

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -6,6 +6,13 @@ Cypress.Commands.add("fetch", ({ role, portal, page }) => {
     fixture: `${page}.json`,
   }).as("GET");
 
+  cy.intercept(
+    "GET",
+    `https://www.googleapis.com/calendar/v3/calendars/${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR}/events?key=${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_API_KEY}&singleEvents=true&orderBy=startTime`,
+    {
+      fixture: `${page}.json`,
+    }
+  ).as("GET");
   cy.visit("/");
   cy.wait("@session");
 
