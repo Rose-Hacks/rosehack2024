99d2a300868a1e80f16d36a23f5f6c59b21715c5
Divyank Shah <divyank.shah.2016@gmail.com>
2024-01-11 01:01:10 -0800

    fix colors

--- src/components/dynamic/admin/services/contacts/Contact.jsx
+++ src/components/dynamic/admin/services/contacts/Contact.jsx
@@ -40,7 +40,7 @@ const Contact = ({ role, disabled, setDisabled }) => {
   };
 
   return (
-    <div className="grid grid-cols-3 w-1/2 gap-3 text-lg bg-white p-4 rounded">
+    <div className="grid grid-cols-3 w-1/2 gap-3 text-lg bg-rosehack-purple-200 p-4 rounded">
       <div className="flex items-center">
         <BsFillPersonFill className="mx-2" /> {role}
       </div>
@@ -53,7 +53,7 @@ const Contact = ({ role, disabled, setDisabled }) => {
         setUser={setStatus}
       />
 
-      <Button text="copy" color="green" onClick={onClick} disabled={disabled} />
+      <Button text="copy" color="gray" onClick={onClick} disabled={disabled} />
     </div>
   );
 };
